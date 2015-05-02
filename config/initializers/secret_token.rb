932b1f0447fbd4e2b3117790a8671541dfb87ae7
Rebecca Anne Sealfon (La-comadreja) <rebecca_sealfon@qualia-media.com>
2015-05-02 14:22:19 -0400

    create new git repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DisruptNy2015::Application.config.secret_key_base = '0c229ded98a2f345d45e214a222b25f9021940bf1eaf80d08010bf479fef9b5c6228a8e8117e0ab5a8f0225fe10056cfa181d3f64f87fc36585d73c801f7521a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DisruptNy2015::Application.config.session_store :cookie_store, key: '_disrupt-ny-2015_session'
